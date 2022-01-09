7e99aab885a7d343874a1d50bdc8b283065d644f
bio666 <claytlotr@gmail.com>
2022-01-09 15:28:00 +0700

    Belajar Laravel 8 | 8. Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,7 @@
 @section('container')
 <h1 class="mb-5">{{ $post["title"] }}</h1>
 
+<p>By Hamas Afif Aswari in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
 {!! $post->body !!}
 <p></p>
