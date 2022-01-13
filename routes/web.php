61a106872f87cdbb7ed944a531b564f1225f5069
bio666 <claytlotr@gmail.com>
2022-01-13 20:42:26 +0700

    Belajar Laravel 8 | 14. View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 use App\Models\Category;
-use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,4 +49,8 @@ Route::get('/categories', function() {
         'categories' => Category::all()
     ]);
 });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
 
