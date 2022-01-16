bcd5f04641a22ab5beedc8f483d3b6c82698e2c9
bio666 <claytlotr@gmail.com>
2022-01-16 12:47:50 +0700

    Belajar Laravel 8 | 20. Update & Delete Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -8,7 +8,7 @@
 
 
     @if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show col-lg-8" role="alert">
       <strong>Congratulation!</strong> {{ session('success') }}
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
@@ -35,10 +35,13 @@
                 <td>
                     <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info" ><span data-feather="eye"></span>
                     </a>
-                    <a href=""class="badge bg-warning"><span data-feather="edit"></span>
+                    <a href="/dashboard/posts/{{ $post->slug }}/edit"class="badge bg-warning"><span data-feather="edit"></span>
                     </a>
-                    <a href=""class="badge bg-danger"><span data-feather="x-circle"></span>
-                    </a>
+                    <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure ?')"><span data-feather="x-circle"></span></button>
+                    </form>
 
                 </td>
               </tr>
