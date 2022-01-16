c5a9d2af9e74f5ea09e675c0b6f89a1d5a2d6f4f
bio666 <claytlotr@gmail.com>
2022-01-16 16:47:07 +0700

    Belajar Laravel 8 | 22. Preview, Update & Delete Image

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -48,14 +48,14 @@
 
                 <div class="mb-3">
                     <label for="image" class="form-label">Post Image</label>
-                    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" >
-                  </div>
-                  
+                    <img class="img-preview img-fluid">
+                    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()" >
                   @error('image')
                   <div class="invalid-feedback">
                       {{ $message }}
                   </div>
               @enderror
+            </div>
 
             <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
@@ -85,5 +85,21 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     })
+
+    function previewImage() {
+
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+    imgPreview.src = oFREvent.target.result;
+    }
+    }
+
 </script>
 @endsection
